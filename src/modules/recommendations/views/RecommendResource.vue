fa66548bcfe9000d287bf3528ac3056fe60e73c7
Andrew Tokeley <andrewtokeley@gmail.com>
2023-04-27 10:27:28 +1200

    added recommend

---
+++ src/modules/recommendations/views/RecommendResource.vue
@@ -0,0 +1,11 @@
+<template>
+  <modal-dialog title="Recommend"></modal-dialog>
+</template>
+
+<script>
+import ModalDialog from '@/core/components/ModalDialog.vue'
+export default {
+  name: "recommend-resource",
+  components: { ModalDialog },  
+}
+</script>
